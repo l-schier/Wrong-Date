7aeacaf36d5e6b3f6af1b958b9be6946362f48a4
nukoc18 <43139477+nukoc18@users.noreply.github.com>
2021-05-11 12:03:08 +0200

    feat: Add EntityParts

---
+++ Common/src/main/java/dk/sdu/mmmi/common/data/entityparts/BlindPart.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.common.data.entityparts;
+
+import dk.sdu.mmmi.common.data.Entity;
+import dk.sdu.mmmi.common.data.GameData;
+
+/**
+ *
+ * @author Kaan
+ */
+public class BlindPart implements EntityPart {
+    
+    private boolean isBlinding;
+    private float blindDuration;
+    
+    public BlindPart(float blindDuration){
+        this.blindDuration = blindDuration;
+        this.isBlinding = false;
+    }
+
+    @Override
+    public void process(GameData gameData, Entity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean isIsBlinding() {
+        return isBlinding;
+    }
+
+    public void setIsBlinding(boolean isBlinding) {
+        this.isBlinding = isBlinding;
+    }
+
+    public float getBlindDuration() {
+        return blindDuration;
+    }
+
+    public void setBlindDuration(float blindDuration) {
+        this.blindDuration = blindDuration;
+    }
+    
+}
