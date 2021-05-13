a572495f91eac098ad4e2bacb814810e35ed86bd
anton <antontj@live.dk>
2021-05-13 11:21:50 +0200

    fix: itemservice cleanup

--- Common/src/main/java/dk/sdu/mmmi/common/services/IItemService.java
+++ Common/src/main/java/dk/sdu/mmmi/common/services/IItemService.java
@@ -2,7 +2,6 @@ package dk.sdu.mmmi.common.services;
 
 import dk.sdu.mmmi.common.data.Entity;
 import dk.sdu.mmmi.common.data.GameData;
-import java.awt.Image;
 
 /**
  *
@@ -10,6 +9,4 @@ import java.awt.Image;
  */
 public interface IItemService {
     void useItem(Entity me, GameData gameData);
-    String getDescription();
-    Image getSprite();
 }
