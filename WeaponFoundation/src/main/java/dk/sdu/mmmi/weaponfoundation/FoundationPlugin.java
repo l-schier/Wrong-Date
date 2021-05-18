b67000902a5cb29ef52724aac997e72c60546232
jaco5157 <jacob-dan@hotmail.com>
2021-05-18 17:40:50 +0200

    feat: Add sprite to foundation

--- WeaponFoundation/src/main/java/dk/sdu/mmmi/weaponfoundation/FoundationPlugin.java
+++ WeaponFoundation/src/main/java/dk/sdu/mmmi/weaponfoundation/FoundationPlugin.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.sdu.mmmi.weaponfoundation;
 
 import dk.sdu.mmmi.common.data.Entity;
 import dk.sdu.mmmi.common.data.GameData;
 import dk.sdu.mmmi.common.data.World;
 import dk.sdu.mmmi.common.data.entityparts.BlindPart;
+import dk.sdu.mmmi.common.data.entityparts.HelpPart;
 import dk.sdu.mmmi.common.data.entityparts.InteractPart;
-import dk.sdu.mmmi.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.common.data.entityparts.RenderPart;
 import dk.sdu.mmmi.common.services.IGamePluginService;
 import java.util.Random;
 
@@ -20,6 +16,7 @@ import java.util.Random;
  * @author Kaan
  */
 public class FoundationPlugin implements IGamePluginService {
+    private final String spriteFile = "foundation.png", helpFile = "foundation.txt";
 
     @Override
     public void start(GameData gameData, World world) {
@@ -37,10 +34,11 @@ public class FoundationPlugin implements IGamePluginService {
         boolean interactable = true;
         foundation.setRadius(8);
 
-        //foundation.add(new InformationPart("testWeapon.png", "description.txt", foundation));
         foundation.add(new PositionPart(x, y));
         foundation.add(new BlindPart(duration));
         foundation.add(new InteractPart(interactable));
+        foundation.add(new RenderPart(this.spriteFile, foundation));
+        foundation.add(new HelpPart(helpFile, foundation));
 
         return foundation;
     }
